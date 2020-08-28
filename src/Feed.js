54f022545b2ec3f093c1d37a5469b7f81387307f
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-08-29 00:56:00 +0600

    facebook message component

--- src/Feed.js
+++ src/Feed.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import "./Feed.css"
 import StoryReal from './StoryReal';
+import MessageSender from './MessageSender';
 
 const Feed = () => {
     return (
         <div className="feed">
             <StoryReal />
+            <MessageSender />
         </div>
     );
 };
