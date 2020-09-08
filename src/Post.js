1fe8f974b243ae8980945a68d94491196d12958c
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-09-08 12:39:41 +0600

    post component

--- src/Post.js
+++ src/Post.js
@@ -2,6 +2,13 @@ import React from 'react';
 import './Post.css';
 import { Avatar } from '@material-ui/core';
 
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import NearMeIcon from '@material-ui/icons/NearMe';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import ExpandMoreOutlined from '@material-ui/icons/ExpandMoreOutlined';
+
+
 const Post = ({profilePic, image, username, timestamp,message}) => {
     return (
         <div className="post">
@@ -13,12 +20,34 @@ const Post = ({profilePic, image, username, timestamp,message}) => {
                     {/* <p> {new Date(timestamp?.toDate().toUTCString)} </p> */}
                 </div>
             </div>
+
             <div className="post__bottom">
                 <p> {message} </p>
             </div>
+
             <div className="post__image">
                 <img src={image} alt=""/>
             </div>
+
+            <div className="post__options">
+                <div className="post__option">
+                    <ThumbUpIcon />
+                    <p> Like </p>
+                </div>
+                <div className="post__option">
+                    <ChatBubbleOutlineIcon />
+                    <p> Comment </p>
+                </div>
+                <div className="post__option">
+                    <NearMeIcon />
+                    <p> Share </p>
+                </div>
+                <div className="post__option">
+                    <AccountCircleIcon />
+                    <ExpandMoreOutlined />
+                </div>
+            </div>
+
         </div>
     );
 };
