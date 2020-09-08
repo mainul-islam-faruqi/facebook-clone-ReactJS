68357fbe9d9d54abd413d2cc645a2e0048e1f203
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-09-08 15:31:42 +0600

    complete the post design

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
+import Widgets from './Widgets';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           {/* Feed */}
           <Feed />
           {/* widgets */}
+          <Widgets />
       </div>
         
     </div>
---
+++ src/Widgets.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./Widgets.css"
+
+const Widgets = () => {
+    return (
+        <div className="widgets">
+            <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FCrazy-Python-Lover-100975844619651%2F&tabs=timeline&width=340&height=1500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" 
+            width="270px"
+            height="100%" 
+            style={{border:"none",overflow:"hidden"}}
+            scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media">
+
+            </iframe>
+        </div>
+    );
+};
+
+export default Widgets;
