a1bbc56b0e6e48532d611d94bed24e6493d1cda0
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-09-08 16:04:50 +0600

    add login information

--- src/App.js
+++ src/App.js
@@ -4,23 +4,32 @@ import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
 import Widgets from './Widgets';
+import Login from './Login';
 
 function App() {
+  const user = null;
   return (
     // BEM namin convention
     <div className="app">
-      {/* Header */}
-      <Header></Header>
-
-      {/* App body */}
-      <div className="app__body">
-          {/* Sidebar */}
-          <Sidebar style={{width:"600px"}}></Sidebar>
-          {/* Feed */}
-          <Feed />
-          {/* widgets */}
-          <Widgets />
-      </div>
+      {
+        user ? (
+          <>
+          {/* Header */}
+          <Header></Header>
+    
+          {/* App body */}
+          <div className="app__body">
+              {/* Sidebar */}
+              <Sidebar style={{width:"600px"}}></Sidebar>
+              {/* Feed */}
+              <Feed />
+              {/* widgets */}
+              <Widgets />
+          </div>
+        </>
+        ) : <Login/>
+        
+      }
         
     </div>
   );
--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 const Login = () => {
     return (
         <div className="login">
-            Log In
+            <div className="login__top">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSDnQr7_YTbHdheXzINJsubSxaRBOy1_0Ikug&usqp=CAU" alt=""/>
+                <img src="https://download.logo.wine/logo/Facebook/Facebook-Logo.wine.png" alt=""/>
+            </div>
+            
+
+            <Button type="submit" onClick="">
+                Sign In
+            </Button>
         </div>
     );
 };
